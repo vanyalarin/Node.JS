08c2a8b8d822ff4f5bcd6ea4a2cd4f58e77445f1
vanyalarin <vanyalarin2000@gmail.com>
2020-01-30 14:01:00 +0200

    Hello World

---
+++ firstapp/app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+http.createServer(function(request,response){
+    response.end("Hello World");
+}).listen(3000, "127.0.0.1", function(){
+    console.log("Сервер начал прослушивание запросов на порту 3000")
+});
